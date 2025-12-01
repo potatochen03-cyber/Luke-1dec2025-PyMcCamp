b7c3e76d8ee283f1c5420e3dbc0369cfb4fb958f
potatochen03-ai <potatochen03@gmail.com>
2025-12-01 08:53:51 +0000

    Dec 1, 2025, 8:53 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -4,38 +4,54 @@ print("hello from day1")
 
 ########################################################################
 # Task 1:
-
+print("hello from day1")
 
 
 ########################################################################
 # Task 2:
-
-
-
+print("my name is Luke Chen")
+print("I an 12 years old")
+print("My school is St Andrews Junior School, but am going to Victoria Secondary")
+print("If I had $2,000,I would save it for when i am older")
 ########################################################################
 # Task 3:
-
-
-
+animal2="mouse"
+animal3="tiger"
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+v1=5
+v2=9
+print(v1+v2)
+print(v2-v1)
+print(v1*v2)
+print(v2/v1)
 ########################################################################
 # Task 5:
-
-
-
+name="Ravi"
+sports="running"
+print("my name is" + name)
+print("I hate" + sports)
+print(name+"is"+sports)
 ########################################################################
 # Task 6:
+def intro():
+    print("my name is Luke Chen")
+    print("I an 12 years old")
+    print("My school is St Andrews Junior School, but am going to Victoria Secondary")
+    print("If I had $2,000,I would save it for when i am older")
 
+intro()
 
 
 ########################################################################
 # Task 7:
+def addition(number1,number2):
+    print(number1+number2)
 
-
-
+addition(2,3)
+addition(243,236)
+addition(5000,3)
 ########################################################################
 # Additional exercises:
