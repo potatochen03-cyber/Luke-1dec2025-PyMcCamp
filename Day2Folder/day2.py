d980befd5bb97dee0e8be2b9dcfeeb09de2d9637
potatochen03-ai <potatochen03@gmail.com>
2025-12-02 06:14:24 +0000

    Dec 2, 2025, 6:14 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -6,7 +6,7 @@ print("hello from day2")
 # Task 1:
 for count in range(100):
     print("i will not sling mud at my friends")
-
+    p
 
 ########################################################################
 # Task 2:
