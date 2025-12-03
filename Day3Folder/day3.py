efca10d60dc712756c063956b132ace3b7beaa1e
potatochen03-ai <potatochen03@gmail.com>
2025-12-03 06:35:41 +0000

    Dec 3, 2025, 6:35 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -10,7 +10,7 @@ print("hello from day3")
 # Task 2:
 name="Luke "
 num_pens=12 
-print(name +"bought "+str(num_pens) +"pens.")
+print(name +"bought "+str(num_pens) +" pens.")
 
 
 ########################################################################
