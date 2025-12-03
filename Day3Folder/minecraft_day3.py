69489829fe7df80a8ae2c87784d50a40602659c5
potatochen03-ai <potatochen03@gmail.com>
2025-12-03 08:51:51 +0000

    Dec 3, 2025, 8:51 AM

--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,14 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def bw():
+    for b in range(31):
+        agent.move(UP, 1)
+        agent.place(DOWN)
+        agent.move(UP, 1)
+        agent.place(DOWN)
+        agent.move(UP, 1)
+        agent.place(DOWN)
+        agent.move(RIGHT, 1)
+        agent.move(DOWN, 4)
+
+player.on_chat("bw",bw)
