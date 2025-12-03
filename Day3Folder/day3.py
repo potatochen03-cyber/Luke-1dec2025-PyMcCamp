57db8be56911a427fe9afdccdd7e6dd42a9bcdd6
potatochen03-ai <potatochen03@gmail.com>
2025-12-03 06:32:36 +0000

    Dec 3, 2025, 6:32 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -4,10 +4,6 @@ print("hello from day3")
 
 ########################################################################
 # Task 1:
-myname = input("what is your name ")
-mytitle = input("what is your title ")
-mycommand = input("what is your command ")
-print(mytitle + myname + "commands the people to " + mycommand)
 
 
 ########################################################################
